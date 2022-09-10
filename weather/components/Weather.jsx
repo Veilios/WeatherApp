035d8dcd808a87f04263067f334b3b9308d8d27f
Alan Mir <alanpaulphotos@gmail.com>
2022-09-09 23:50:11 -0400

    Removed white space at bottom when content populated

--- weather/components/Weather.jsx
+++ weather/components/Weather.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Weather = ({ data }) => {
 
     return (
-        <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10'>
+        <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[80vh] m-auto p-12 text-gray-300 z-10'>
             {/* Top */}
             <div className='relative flex justify-between pt-12'>
                 <div className='flex flex-col items-center'>
@@ -16,7 +16,7 @@ const Weather = ({ data }) => {
                     />
                     <p className='text-2xl'>{data.weather[0].main}</p>
                 </div>
-                <p className='text-9xl'>{data.main.temp.toFixed(0)}&#176;</p>
+                <p className='text-8xl'>{data.main.temp.toFixed(0)}&#176;</p>
             </div>
             {/* Bottom */}
             <div className='bg-black/50 relative p-8 rounded-md'>
